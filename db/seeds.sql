b77f462b7dc9015afa92e9b40d698435d99b9b4a
Jacob Kriese <jkriese12@gmail.com>
2021-02-19 19:29:18 -0700

    Installed node packages, created db, connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Turkey", TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Veggie", TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Bison", FALSE);
